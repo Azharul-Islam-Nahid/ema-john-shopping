8ae3f623919b21470c65e8ec8e123ca73f871787
Azharul-Islam-Nahid <lonewolfazharul@gmail.com>
2022-10-10 15:49:32 +0600

    added react-routes

---
+++ src/Loaders/productsAndCartLoader.js
@@ -0,0 +1,23 @@
+import { getStoredCard } from "../utilities/fakedb";
+
+export const productsAndCartLoader = async () => {
+
+    const productsData = await fetch('products.json');
+    const products = await productsData.json();
+
+    const savedCart = getStoredCard();
+
+    const initialCart = [];
+
+    for (const id in savedCart) {
+        const addedProduct = products.find(product => product.id === id);
+        if (addedProduct) {
+            const quantity = savedCart[id];
+            addedProduct.quantity = quantity;
+            initialCart.push(addedProduct);
+
+        }
+    }
+
+    return { products: products, initialCart: initialCart };
+}
